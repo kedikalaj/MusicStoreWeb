2a51155cc2e46c2fab5abdee11ad6344472f73bd
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-10-16 20:18:25 +0200

    Added pagination

--- MusicStoreWeb/Controllers/MusicController.cs
+++ MusicStoreWeb/Controllers/MusicController.cs
@@ -5,18 +5,33 @@ using System.Web;
 using System.Web.Mvc;
 using MusicStore.Domain.Abstract;
 using MusicStore.Domain.Entities;
+using MusicStoreWeb.Models;
+
 namespace SportsStore.WebUI.Controllers
 {
     public class MusicController : Controller
     {
         private ISongsRepository repository;
+        public int PageSize = 4;
         public MusicController(ISongsRepository productRepository)
         {
             this.repository = productRepository;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Songs.ToList());
-        }
+            SongsListViewModel model = new SongsListViewModel
+            {
+                Songs = repository.Songs
+            .OrderBy(p => p.SongID)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize), PagingInfo = new PagingInfo{
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Songs.Count()
+                }
+        };
+
+            return View(model);
     }
+}
 }
---
+++ MusicStoreWeb/Models/SongsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MusicStore.Domain.Entities;
+
+
+namespace MusicStoreWeb.Models
+{
+    public class SongsListViewModel
+    {
+        public IEnumerable<Song> Songs { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
