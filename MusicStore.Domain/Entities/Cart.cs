9fb2427bb619458242bcb9e436ea64d520f6e99c
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-10-17 22:52:53 +0200

    Builded the basic functionality of adding products to the cart

---
+++ MusicStore.Domain/Entities/Cart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+        public void AddItem(Song product, int quantity)
+        {
+            CartLine line = lineCollection
+            .Where(p => p.Product.SongID == product.SongID)
+            .FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public void RemoveLine(Song product)
+        {
+            lineCollection.RemoveAll(l => l.Product.SongID == product.SongID);
+        }
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.Product.Price * e.Quantity);
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+    public class CartLine
+    {
+        public Song Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
+
---
+++ MusicStoreWeb/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MusicStore.Domain.Abstract;
+using MusicStore.Domain.Entities;
+using MusicStoreWeb.Models;
+
+namespace MusicStoreWeb.Controllers
+{
+    public class CartController : Controller
+    {
+        private ISongsRepository repository;
+        public CartController(ISongsRepository repo)
+        {
+            repository = repo;
+        }
+        public RedirectToRouteResult AddToCart(int songId, string returnUrl)
+        {
+            Song product = repository.Songs
+            .FirstOrDefault(p => p.SongID == songId);
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Song product = repository.Songs
+            .FirstOrDefault(p => p.SongID == productId);
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+        return RedirectToAction("Index", new { returnUrl });
+        }
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+    }
+}
