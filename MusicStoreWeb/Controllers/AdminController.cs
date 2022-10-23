86f1c4ddfaa7543b9f48d0986677cc646ea66304
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-10-23 22:43:02 +0200

    Added edit and delete controller and view

---
+++ MusicStoreWeb/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Domain.Abstract;
+using System.Linq;
+using MusicStore.Domain.Entities;
+
+namespace MusicStoreWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ISongsRepository repository;
+        public AdminController(ISongsRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.Songs);
+        }
+        public ViewResult Edit(int SongsId)
+        {
+            Song product = repository.Songs
+            .FirstOrDefault(p => p.SongID == SongsId);
+            return View(product);
+        }
+    }
+}
