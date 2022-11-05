40076993a688949cfbef7c9627123c428a0205fb
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-11-05 02:52:25 +0100

    Added repositories for the Entity Framework classes and added the dependencies

---
+++ MusicStore.Domain/Abstract/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MusicStore.Domain.Entities;
+using System.Data.Entity;
+
+namespace MusicStore.Domain.Abstract
+{
+    public interface IRoleRepository
+    {
+        IEnumerable<Role> Role { get; }
+
+
+    }
+}
---
+++ MusicStore.Domain/Concrete/EFRoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicStore.Domain.Abstract;
+using MusicStore.Domain.Entities;
+
+namespace MusicStore.Domain.Concrete
+{
+    public class EFRoleRepository : IRoleRepository
+    {
+        public EFDbContext context = new EFDbContext();
+        public IEnumerable<Role> Role
+        {
+            get { return context.Role; }
+        }
+    }
+}
