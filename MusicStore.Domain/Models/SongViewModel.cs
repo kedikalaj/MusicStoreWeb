d7bc9f62dd31a7ed9c936ac795c3ed600e9c080b
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-11-21 21:26:02 +0100

    Added validation to songViewModel

--- MusicStore.Domain/Models/SongViewModel.cs
+++ MusicStore.Domain/Models/SongViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,15 @@ namespace MusicStore.Domain.Models
     public class SongViewModel
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Please enter a name for the song")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter an author for the song")]
         public string Author { get; set; }
+        [Required(ErrorMessage = "Please enter a price for the song")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Please enter a genre id for the song")]
         public int GenreID { get; set; }
+        [Required(ErrorMessage = "Please enter a length for the song")]
         public int Length { get; set; }
         public byte[] ImageData { get; set; }
         public string ImageMimeType { get; set; }
