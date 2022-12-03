4e1eb39a440224472b5cc4add4ea560beb35821c
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-12-03 22:48:35 +0100

    Added the model and controller for the user login

---
+++ MusicStoreWeb/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Domain.Entities;
+using MusicStore.Domain.Abstract;
+using MusicStore.Domain.Models;
+
+
+namespace MusicStoreWeb.Controllers
+{
+    public class UserController : Controller
+    {
+
+       IUserRepository userRepository;
+        public UserController(IUserRepository userRepo)
+        {
+            userRepository = userRepo;
+        }
+
+
+        public ActionResult UserLogin()
+        {
+            return View();
+        }
+    }
+}
